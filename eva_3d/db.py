1a743359cf65a3f6893cfaf9cf3b905eaae13408
Paweł Kucmus <pkucmus@gmail.com>
2021-12-20 17:26:33 +0100

    leverage python-onshape-exporter

--- eva_3d/db.py
+++ eva_3d/db.py
@@ -1,206 +1,49 @@
-import sqlite3
-from sqlite3 import Error
+from typing import Optional, List
+import warnings
+
+from sqlalchemy import exc as sa_exc
+from sqlmodel import Field, SQLModel, create_engine, Relationship
+from sqlmodel.sql.expression import select
 
 
-def get_db_connection():
-    conn = None
-    try:
-        conn = sqlite3.connect(":memory:")
-        # conn = sqlite3.connect('db.sqlite3')
-        print(sqlite3.version)
-        return conn
-    except Error as e:
-        print(e)
+warnings.simplefilter("ignore", category=sa_exc.SAWarning)
+
+class BOMTable(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    items: List["BOMItem"] = Relationship(back_populates="bom_table")
 
 
-def initialize_db(conn):
-    cur = conn.cursor()
-    cur.execute(
-        """
-        CREATE TABLE IF NOT EXISTS bom (
-            id INTEGER PRIMARY KEY,
-            file_path TEXT,
-            namespace TEXT
-        );
-    """
-    )
-    cur.execute(
-        """
-        CREATE TABLE IF NOT EXISTS item_entry (
-            id INTEGER PRIMARY KEY,
-            name TEXT,
-            qty INTEGER,
-            type TEXT,
-            url TEXT,
-            bom_id INTEGER,
-            FOREIGN KEY (bom_id) REFERENCES bom (id)
-        );
-    """
-    )
-    cur.execute(
-        """
-        CREATE TABLE IF NOT EXISTS vendor_mapping (
-            id INTEGER PRIMARY KEY,
-            vendor_name TEXT,
-            eva_part_name TEXT,
-            eva_part_type TEXT,
-            vendor_part_name TEXT,
-            vendor_sku TEXT,
-            vendor_ignore INTEGER,
-            FOREIGN KEY (eva_part_name) REFERENCES item_entry (name)
-        );
-    """
-    )
+class BOMItem(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    description: Optional[str]
+    material: str
+    quantity: float
+    bom_table_id: Optional[int] = Field(default=None, foreign_key="bomtable.id")
+    bom_table: Optional[BOMTable] = Relationship(back_populates="items")
+
+    @property
+    def is_printable(self):
+        return self.material.upper() == "PETG"
 
 
-def create_bom(conn, file_path, namespace):
-    cur = conn.cursor()
-    cur.execute(
-        """
-        INSERT INTO bom (
-            file_path,
-            namespace
-        ) VALUES (?,?)
-    """,
-        (file_path, namespace),
-    )
-    conn.commit()
-    return cur.lastrowid
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+# engine = create_engine(sqlite_url, echo=True)
+engine = create_engine(sqlite_url)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
 
 
-def create_item_entry(conn, bom_id, name, qty, type, url):
-    cur = conn.cursor()
-    cur.execute(
-        """
-        INSERT INTO item_entry (
-            bom_id,
-            name,
-            qty,
-            type,
-            url
-        ) VALUES (?,?,?,?,?)
-    """,
-        (bom_id, name, qty, type, url),
-    )
-    conn.commit()
-    return cur.lastrowid
+def remove_page_bom(session, page_uid):
+    tables = session.exec(select(BOMTable).where(BOMTable.name == page_uid)).all()
+    for table in tables:
+        items = session.exec(select(BOMItem).where(BOMItem.bom_table_id == table.id)).all()
+        for item in items:
+            session.delete(item)
+        session.delete(table)
 
-
-def get_items_by_bom_id(conn, bom_id):
-    cur = conn.cursor()
-    cur.execute(
-        """
-        SELECT 
-            name,
-            SUM(qty) qty,
-            type,
-            url
-        FROM item_entry 
-        WHERE bom_id = ?
-        GROUP BY name
-    """,
-        (bom_id,),
-    )
-    return cur.fetchall()
-
-
-def create_vendor_mapping(
-    conn,
-    vendor_name,
-    eva_part_name,
-    eva_part_type,
-    vendor_part_name,
-    vendor_sku,
-    vendor_ignore,
-):
-    cur = conn.cursor()
-    cur.execute(
-        """
-        INSERT INTO vendor_mapping (
-            vendor_name,
-            eva_part_name,
-            eva_part_type,
-            vendor_part_name,
-            vendor_sku,
-            vendor_ignore
-        ) VALUES (?,?,?,?,?,?)
-    """,
-        (
-            vendor_name,
-            eva_part_name,
-            eva_part_type,
-            vendor_part_name,
-            vendor_sku,
-            1 if vendor_ignore else 0,
-        ),
-    )
-    conn.commit()
-    return cur.lastrowid
-
-
-def truncate_vendor_mapping(conn):
-    cur = conn.cursor()
-    cur.execute("""DELETE FROM vendor_mapping""")
-    conn.commit()
-
-
-def get_vendors(conn):
-    cur = conn.cursor()
-    cur.execute(
-        """
-        SELECT vendor_name
-        FROM vendor_mapping 
-        GROUP BY vendor_name
-    """,
-    )
-    return cur.fetchall()
-
-
-def get_superbom(conn):
-    cur = conn.cursor()
-    cur.execute(
-        """
-        SELECT 
-            namespace,
-            name,
-            SUM(qty) qty,
-            type,
-            url
-        FROM item_entry 
-        INNER JOIN bom ON bom.id = item_entry.bom_id
-        GROUP BY namespace, name
-    """,
-    )
-    return cur.fetchall()
-
-
-def get_vendor_superbom(conn, vendor_name):
-    cur = conn.cursor()
-    cur.execute(
-        """
-        SELECT
-            sub.name,
-            SUM(sub.qty) qty,
-            sub.type,
-            sub.vendor_part_name,
-            sub.vendor_sku
-        FROM (
-            SELECT 
-                namespace,
-                name,
-                MAX(qty) qty,
-                type,
-                vendor_part_name,
-                vendor_sku
-            FROM item_entry 
-            INNER JOIN bom ON bom.id = item_entry.bom_id
-            LEFT JOIN vendor_mapping ON vendor_mapping.eva_part_name = item_entry.name
-            WHERE vendor_name = ? AND vendor_ignore = 0
-            GROUP BY namespace, name
-        ) AS sub
-        WHERE sub.type = "hardware"
-        GROUP BY name
-    """,
-        (vendor_name,),
-    )
-    return cur.fetchall()
+    session.commit()
